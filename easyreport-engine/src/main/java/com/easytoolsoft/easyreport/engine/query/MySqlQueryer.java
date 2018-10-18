b10c52c623048f27fe41eca57193fd2727520e64
yanhan <465433803@qq.com>
2018-10-18 17:12:47 +0800

    2018-10-18 1、mysql查询limit限制解除 2、添加测试数据

--- easyreport-engine/src/main/java/com/easytoolsoft/easyreport/engine/query/MySqlQueryer.java
+++ easyreport-engine/src/main/java/com/easytoolsoft/easyreport/engine/query/MySqlQueryer.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 import com.easytoolsoft.easyreport.engine.data.ReportDataSource;
 import com.easytoolsoft.easyreport.engine.data.ReportParameter;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -18,11 +19,12 @@ public class MySqlQueryer extends AbstractQueryer implements Queryer {
     @Override
     protected String preprocessSqlText(String sqlText) {
         sqlText = StringUtils.stripEnd(sqlText.trim(), ";");
-        final Pattern pattern = Pattern.compile("limit.*?$", Pattern.CASE_INSENSITIVE);
-        final Matcher matcher = pattern.matcher(sqlText);
-        if (matcher.find()) {
-            sqlText = matcher.replaceFirst("");
-        }
+//        18-10-16 yanh sql中需要limit作为参数进行查询，故注释
+//        final Pattern pattern = Pattern.compile("limit.*?$", Pattern.CASE_INSENSITIVE);
+//        final Matcher matcher = pattern.matcher(sqlText);
+//        if (matcher.find()) {
+//            sqlText = matcher.replaceFirst("");
+//        }
         return sqlText;
     }
 }
